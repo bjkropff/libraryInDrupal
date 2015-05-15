297df95e0b1f22150eaa99f5f9215f1165664dc2
Brian <brian.kropff@gmail.com>
2015-05-15 10:47:11 -0700

    Add the created feature and recreated feature

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -212,7 +212,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'library',
+      'username' => 'librarian',
+      'password' => 'password',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -244,7 +258,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = '5PNwWDlPpt4MIYlsCLFpsbsdFtdN4cH3JitRk-e0HAM';
 
 /**
  * Base URL (optional).
